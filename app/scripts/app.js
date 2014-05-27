360b735d2b4af6f114dc0dd835b9632769ad7db7
Rafael Oliveira Andrade <rafael.andrade@tuntscorp.com>
2014-05-27 17:32:58 -0300

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+  .module('voppBackOfficeApp', [
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
